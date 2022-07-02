47288a7ef306da94d02dec413f28b5a1548f4a89
Artem Mukhamatyanov <ausupertramp@gmail.com>
2022-07-02 15:11:27 +1000

    removed console.log

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const isToday = (someDate) => {
 
 (async () => {
     try {
-        bot.sendMessage('@mytestrsschannel', `Проверка новостей`);
         const feed = await parser.parseURL('https://www.jw-russia.org/rss.xml');
   
         feed.items.reverse().forEach(({ guid, title, link, isoDate }, index) => {
